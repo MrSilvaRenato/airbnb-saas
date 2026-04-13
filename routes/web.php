5bc8d5b456016b458f4e209bffbe13e6e8ef0454
Renato <37394601+MrSilvaRenato@users.noreply.github.com>
2026-04-13 17:14:01 +1000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -103,7 +103,7 @@ Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::post('/profile', [ProfileController::class, 'update']); // fallback
 Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+
 });
 
 
